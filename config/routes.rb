3e25bceaa592642ee02a51abb06531ad0e4285d3
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 15:20:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index,:show,:create]
+  resources :students, only: [:index,:show,:new,:create]
   # get '/students/:id', to: 'students#show', as: 'student'
 
 end
