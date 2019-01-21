309c33c4b67b67e6acfc09d03604e835e827448a
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 15:18:32 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,11 +4,13 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def new
+  end
+
+
   def show
     @student = Student.find(params[:id])
   end
 
-  def new
-  end
 
 end
