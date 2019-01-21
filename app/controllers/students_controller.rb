dcf039f047916cb30e9d3fb8930d127bde39c4d0
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 15:50:10 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,7 +14,7 @@ class StudentsController < ApplicationController
 
   def create
     @student=Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to sow
+    redirect_to show
   end
 
 
