9bcc8c072f8442e947979ee88c06a1715f5680eb
Gilbert Torchon <gilbert@noukod.com>
2019-01-21 15:47:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,8 +13,8 @@ class StudentsController < ApplicationController
   end
 
   def create
-    Student.create(first_name: params[:first_name], last_name: params[:last_name])
-    redirect_to new_student_path
+    @student=Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    erb :"students/view"
   end
 
 
